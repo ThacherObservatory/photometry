329c804339e2e5cee040b1470a86dcb20372c0a4
Yousef Lawrence <ylawrence@thacher.org>
2016-04-02 22:57:17 -0700

    added flats for the 4 photometric bands

--- clusterphot.py
+++ clusterphot.py
@@ -23,16 +23,28 @@ if path == 'Yousef':
 #if path == 'Alden':
     #Alden, input your path here
 
+#test invidiual image
+qi.readimage('n20160112.T4.NGC188.gp.0096.fits')
 
 files = glob.glob(path+"*fits")
 
 #image calibration (bias, dark,flat)
-darks,dct = tp.get_files(tag='Dark')
-biases,bct = tp.get_files(tag='Bias')
-flats,fct = tp.get_files(tag='SkyFlat')
+biases,bct = tp.get_files(dir = path, tag='Bias')
+darks,dct = tp.get_files(dir = path,tag='Dark')
+#flats,fct = tp.get_files(dir = path, tag='SkyFlat')
+gflats = tp.get_files(dir = path, tag = 'SkyFlat.gp')
+iflats = tp.get_files(dir = path, tag = 'SkyFlat.ip')
+rflats = tp.get_files(dir = path, tag = 'SkyFlat.rp')
+zflats = tp.get_files(dir = path, tag = 'SkyFlat.zp')
 
-bias = tp.master_bias(biases,outdir='./Photometry/',readnoise=False)
+bias = tp.master_bias(biases,outdir='./Photometry/')
 
 dark = tp.master_dark(darks,bias=bias,outdir='./Photometry/')
 
-flat = tp.master_flat(flats,bias=bias,dark=dark,outdir='./Photometry/')
+gflat = tp.master_flat(gflats,bias=bias,dark=dark,outdir='./Photometry/')
+iflat = tp.master_flat(iflats,bias=bias,dark=dark,outdir='./Photometry/')
+rflat = tp.master_flat(rflats,bias=bias,dark=dark,outdir='./Photometry/')
+zflat = tp.master_flat(zflats,bias=bias,dark=dark,outdir='./Photometry/')
+
+#below is just the basic calibrated image equation
+cal = (image - dark - bias)/flat

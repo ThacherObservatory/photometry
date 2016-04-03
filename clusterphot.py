dd31a1bc361e66c83ff2350aedc3c598a47a9716
Yousef Lawrence <ylawrence@thacher.org>
2016-04-02 17:08:19 -0700

    added unedited image calibration code

--- clusterphot.py
+++ clusterphot.py
@@ -10,15 +10,32 @@ from astropy.visualization import scale_image
 from photutils import CircularAperture, aperture_photometry, CircularAnnulus
 import quickimage as qi
 
-if path == 'Shin'
-    //Shin's path
-if path == 'Yousef'
-    //Yousef's path
-if path == 'Alden'
-    //Alden's path
+#Yousef notes: for me at least there is a problem with import statements,
+#particularly with thacherphot not being able to import the module fitgaussian
+
+#if path == 'Shin':
+    #Shin's path
+if path == 'Yousef':
+    path = '/Users/Yousef/Desktop/Astronomy/MINERVA Star Clusters/'
+#if path == 'Alden':
+    #Alden's path
 
 
 files = glob.glob(path+"*fits")
+#test image in files
+qi.readimage(files[10],plot=True)
 
 image,header = qi.readimage(files[50],plot=True,siglo=2,sighi=2)
 w = wcs.WCS(header)
+
+
+#image calibration (bias, dark,flat)
+darks,dct = tp.get_files(tag='Dark')
+biases,bct = tp.get_files(tag='Bias')
+flats,fct = tp.get_files(tag='SkyFlat')
+
+bias = tp.master_bias(biases,outdir='./Photometry/',readnoise=False)
+
+dark = tp.master_dark(darks,bias=bias,outdir='./Photometry/')
+
+flat = tp.master_flat(flats,bias=bias,dark=dark,outdir='./Photometry/')

aae0905bd87b0894cddcd9eb5cef3c57cdd5cdb8
Yousef Lawrence <ylawrence@thacher.org>
2016-04-05 21:31:59 -0700

    expanded on image calibration for the 4 photometric bands

--- clusterphot.py
+++ clusterphot.py
@@ -23,12 +23,14 @@ if path == 'Yousef':
 #if path == 'Alden':
     #Alden, input your path here
 
+#########################################################################
+
 #test invidiual image
 qi.readimage('n20160112.T4.NGC188.gp.0096.fits')
 
 files = glob.glob(path+"*fits")
 
-#image calibration (bias, dark,flat)
+#image calibration using biases
 biases,bct = tp.get_files(dir = path, tag='Bias')
 darks,dct = tp.get_files(dir = path,tag='Dark')
 #flats,fct = tp.get_files(dir = path, tag='SkyFlat')
@@ -41,10 +43,18 @@ bias = tp.master_bias(biases,outdir='./Photometry/')
 
 dark = tp.master_dark(darks,bias=bias,outdir='./Photometry/')
 
+#master flats for the 4 photometric bands
 gflat = tp.master_flat(gflats,bias=bias,dark=dark,outdir='./Photometry/')
 iflat = tp.master_flat(iflats,bias=bias,dark=dark,outdir='./Photometry/')
 rflat = tp.master_flat(rflats,bias=bias,dark=dark,outdir='./Photometry/')
 zflat = tp.master_flat(zflats,bias=bias,dark=dark,outdir='./Photometry/')
 
-#below is just the basic calibrated image equation
-cal = (image - dark - bias)/flat
+#calibrated image equations for the 4 photometric bands
+gcal = (image - dark - bias)/gflat
+ical = (image - dark - bias)/iflat
+rcal = (image - dark - bias)/rflat
+zcal = (image - dark - bias)/zflat
+
+
+
+

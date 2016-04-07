682f8e4e0431615fa2a46468852f4f168490b308
Yousef Lawrence <ylawrence@thacher.org>
2016-04-06 21:04:00 -0700

    streamlined code and comments

--- clusterphot.py
+++ clusterphot.py
@@ -8,57 +8,65 @@ from astropy.coordinates import SkyCoord
 from astropy import units as u
 from astropy.visualization import scale_image
 from photutils import CircularAperture, aperture_photometry, CircularAnnulus
-#import quickimage as qi
+import quick_image as qi
+import sys
 
 #to change the path, just change the assignment below to your name (but don't forget to 
 #actually type in your path in the if statements, as well as any potential import statements)
 path = 'Yousef'
 
-#if path == 'Shin':
+if path == 'Shin':
     path= '/Users/shinnosuke/Documents/Astronomy/__MACOSX/2016Jan12/'
     #path= 'Users/shinnosuke/Documents/Astronomy/__MACOSX/2016Jan13/'
-    import swiftimage as qi
+    #import swiftimage as qi
 if path == 'Yousef':
     path = '/Users/Yousef/Desktop/Astronomy/MINERVA Star Clusters/2016Jan12/'
     #path = '/Users/Yousef/Desktop/Astronomy/MINERVA Star Clusters/2016Jan13/'
-    import swiftimage as qi
+    
 #if path == 'Alden':
     #Alden, input your path here
 
 #########################################################################
 
-#test invidiual image
-image, header = fits.getdata(path + 'n20160112.T4.NGC188.gp.0096.fits', 0, header = True)
+#get raw image
+gimage, gheader = fits.getdata(path + 'n20160112.T4.NGC188.gp.0096.fits', 0, header = True)
+rimage, rheader = fits.getdata(path + 'n20160112.T4.NGC188.rp.0107.fits', 0, header = True)
+iimage, iheader = fits.getdata(path + 'n20160112.T4.NGC188.ip.0121.fits', 0, header = True)
+zimage, zheader = fits.getdata(path + 'n20160112.T4.NGC188.zp.0140.fits', 0, header = True)
 
+#get files
 files = glob.glob(path+"*fits")
 
-#image calibration using biases
+#get bias,dark,flats files
 biases,bct = tp.get_files(dir = path, tag='Bias')
 darks,dct = tp.get_files(dir = path,tag='Dark')
-#flats,fct = tp.get_files(dir = path, tag='SkyFlat')
 gflats = tp.get_files(dir = path, tag = 'SkyFlat.gp')
 iflats = tp.get_files(dir = path, tag = 'SkyFlat.ip')
 rflats = tp.get_files(dir = path, tag = 'SkyFlat.rp')
 zflats = tp.get_files(dir = path, tag = 'SkyFlat.zp')
 
+#create master bias,flat
 bias = tp.master_bias(biases,outdir='./')
-
 dark = tp.master_dark(darks,bias=bias,outdir='./')
 
 #master flats for the 4 photometric bands
-gflat = tp.master_flat(gflats[0],bias=bias,dark=dark,outdir='./')
-iflat = tp.master_flat(iflats[0],bias=bias,dark=dark,outdir='./')
-rflat = tp.master_flat(rflats[0],bias=bias,dark=dark,outdir='./')
-zflat = tp.master_flat(zflats[0],bias=bias,dark=dark,outdir='./')
+gflat = tp.master_flat(gflats[0],bias=bias,dark=dark,outdir='./', suffix = 'gp')
+rflat = tp.master_flat(rflats[0],bias=bias,dark=dark,outdir='./', suffix = 'rp')
+iflat = tp.master_flat(iflats[0],bias=bias,dark=dark,outdir='./', suffix = 'ip')
+zflat = tp.master_flat(zflats[0],bias=bias,dark=dark,outdir='./', suffix = 'zp')
 
 #calibrated image equations for the 4 photometric bands
-#note: image will be defined later (when a specific image is chosen)
-gcal = (image - dark - bias)/gflat
-ical = (image - dark - bias)/iflat
-rcal = (image - dark - bias)/rflat
-zcal = (image - dark - bias)/zflat
+gcal = (gimage - dark - bias)/gflat
+rcal = (rimage - dark - bias)/rflat
+ical = (iimage - dark - bias)/iflat
+zcal = (zimage - dark - bias)/zflat
 
-qi.readimage(image)
+#display images
+qi.display_image(gcal,siglo = 2, sighi = 2,fignum = 2)
+qi.display_image(rcal,siglo = 2, sighi = 2,fignum = 2)
+qi.display_image(ical,siglo = 2, sighi = 2,fignum = 2)
+qi.display_image(zcal,siglo = 2, sighi = 2,fignum = 2)
+
 
 
 

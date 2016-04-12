38a0b92754a3a3b19d5b36e63b305cda1c5ffd91
Yousef Lawrence <ylawrence@thacher.org>
2016-04-11 22:21:33 -0700

    added outline for performing photometry on a single star

--- clusterphot.py
+++ clusterphot.py
@@ -11,6 +11,7 @@ from photutils import CircularAperture, aperture_photometry, CircularAnnulus
 import quick_image as qi
 import sys
 
+
 #to change the path, just change the assignment below to your name (but don't forget to 
 #actually type in your path in the if statements, as well as any potential import statements)
 path = 'Yousef'
@@ -25,6 +26,7 @@ if path == 'Yousef':
     #Alden, input your path here
 
 #-------------------------------------------------------------------------#
+#-------------------------------------------------------------------------#
 
 #get raw image
 gimage, gheader = fits.getdata(path + 'n20160112.T4.NGC188.gp.0096.fits', 0, header = True)
@@ -35,6 +37,7 @@ zimage, zheader = fits.getdata(path + 'n20160112.T4.NGC188.zp.0140.fits', 0, hea
 #get files
 files = glob.glob(path+"*fits")
 
+#Image Calibration -------------------------------------------------------#
 #get bias,dark,flats files
 biases,bct = tp.get_files(dir = path, tag='Bias')
 darks,dct = tp.get_files(dir = path,tag='Dark')
@@ -53,7 +56,7 @@ rflat = tp.master_flat(rflats[0],bias=bias,dark=dark,outdir='./', suffix = 'rp')
 iflat = tp.master_flat(iflats[0],bias=bias,dark=dark,outdir='./', suffix = 'ip')
 zflat = tp.master_flat(zflats[0],bias=bias,dark=dark,outdir='./', suffix = 'zp')
 
-#calibrated image equations for the 4 photometric bands
+#calibrated images for the 4 photometric bands
 gcal = (gimage - dark - bias)/gflat
 rcal = (rimage - dark - bias)/rflat
 ical = (iimage - dark - bias)/iflat
@@ -64,6 +67,19 @@ qi.display_image(gcal,siglo = 2, sighi = 2,fignum = 2)
 qi.display_image(rcal,siglo = 2, sighi = 2,fignum = 2)
 qi.display_image(ical,siglo = 2, sighi = 2,fignum = 2)
 qi.display_image(zcal,siglo = 2, sighi = 2,fignum = 2)
+
+#Photometry on a Single Star --------------------------------------------#
+#potential candidate: x ~= 1830, y = 75
+#potential eclipsing binary: x = 66, y = 1350
+
+#plt.plot(image, coordinates)
+
+
+#Combining all images in a photometric band -----------------------------#
+
+
+
+
 
 
 

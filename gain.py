e15324cdff4d37073ce34074a7e74355924620e3
glaw1300 <glawrence@thacher.org>
2016-12-04 22:36:54 -0800

    Should be fully functioning code for finding gain

--- gain.py
+++ gain.py
@@ -8,20 +8,21 @@ Created on Thu Dec  1 21:15:56 2016
 import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
-import robust as rb
+#import robust as rb
 
 def gain(flat1, flat2, masterdark):
-    
+
     Sum = fits.getdata(flat1)+fits.getdata(flat2)-(fits.getdatat(masterdark)*2)
     Dif = fits.getdata(flat1)-fits.getdata(flat2)-(fits.getdata(masterdark)*2)
-    
+
     Sumimg = Sum/2.0
-    
+
     mean = np.mean(Sumimg)
     variance = np.std(Dif)**2/2.0
-    
-    return mean, variance
-    
+    gain = 1/np.polyfit(mean,variance,1)[0]
+
+    return gain
+
 flat1 = np.random.normal(20.7,7,1000) #mean, std, size
 flat2 = np.random.normal(20.7,7,1000)
 masterdark = np.random.normal
